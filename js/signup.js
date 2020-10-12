16296c2afaea36a4c00df12f8eedf0364d7a49dd
Mónica Cristóbal <monica.cristobal@hotmail.com>
2020-10-12 13:48:42 +0200

    versión (no vuelca)

--- js/signup.js
+++ js/signup.js
@@ -3,39 +3,54 @@ function myFunction() {
   document.getElementById("envi").innerHTML = "" + x;
 }
 
+function letradni() {
+
+}
+
+
+
 function birth() {
 var fecha_hoy = new Date;
 var año = fecha_hoy.getFullYear();
 var mes = fecha_hoy.getMonth();
 var dia = fecha_hoy.getDate();
 
-var fecha_user = document.getElementById("birth").value;
+var fecha_user = document.getElementById("birthdate").value;
 var cumple = new Date (fecha_user);
 var fecha_user = cumple.getFullYear();
 var año_user = cumple.getMonth();
 var dia_user = cumple.getDate();
 
+var x = fecha_hoy.getTime();
+var y = cumple.getTime();
 
-var age = (fecha_hoy - fechar_user) / (1000*60*60*24*365);
-console.log(age);
-document.getElementById("edad").innerHTML = age;
+
+
+var fin = (x - y) / (1000*60*60*24*365);
+console.log(fin);
+
+document.getElementById("edad").innerHTML = fin;
 
 }
 
-function contenido() {
-var precio_inicial = 75
-var i = 75 - 3,75
+function contenido () {
 
-for (var i = 0; i == 37,5; i + 5) {
+var porcent = 0;
+var porcentaje = porcent + 5;
+var precio = 75;
+var calculo = (porcentaje * precio) / 100;
+var i = precio - calculo;
 
-  var fila = "<tr>"
-  + "<td>"" + i + ""</td>"
-  + "<td>"" + i + ""</td>"
-  + "</tr>";
+
+for (var i = 0; i = i <= 37.5; i + 5) {
+
+var fila = "<tr>"
++ "<td>" + porcentaje + "</td>
++ "<td>" + i + "</td>"
++ "</tr>";
 
 document.getElementById("contenido").innerHTML += fila;
 }
-
 }
 
 contenido();
